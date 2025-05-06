b37f7ea0985d1e063f96c6b5f78d5ca55d0a3f27
NeTolik12 <denisakimov902@gmail.com>
2025-05-06 12:08:35 +0300

    Fix asset paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/KR2_SM2_PR2/', // Замените на имя вашего репозитория
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
